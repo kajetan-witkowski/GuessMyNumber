5312b5268d26b659fcf2f2151af1133771b2af3b
udemy535 <udemy535@gmail.com>
2020-11-09 23:08:19 +0100

    Implementic Game Logic

--- script.js
+++ script.js
@@ -18,8 +18,12 @@
 // console.log(document.querySelector('.guess').value);
 // //returns 23
 
-////////////////////////
-// 1. Event listener
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+//Math.random() returns value from 0 to 1 if we specify the multiply number we get random number from 0 to 20 in this case
+//Math.trunc() change the returning value to integer 1,5 to 1
+//We also adds 1 to the ending value to increase the value to 20 because we are ending with 19.99999999 at the end
 
 document.querySelector('.check').addEventListener('click', function() {
   // const guess = document.querySelector('.guess').value;
@@ -33,8 +37,27 @@ document.querySelector('.check').addEventListener('click', function() {
   if (!guess) {
     document.querySelector('.message').textContent =
       'No number!🚫 Please input value between 1 and 20';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+  } else if (guess > secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = '📈 Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+    }
+  } else if (guess < secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = '📉 Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+    }
   }
 });
-
 // target.addEventListener(type, listener, useCapture)
+//
 // useCapture accepts True or False (OPTIONAL)
+////////////////////////////////////////////////////////
