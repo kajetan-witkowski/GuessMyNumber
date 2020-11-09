9e735f7983d1b44c829550f6ffe2c2a3d2bb3ac6
udemy535 <udemy535@gmail.com>
2020-11-09 22:28:42 +0100

    First commit adding HTML and CSS styles

---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+// //returns Start quessing
+
+// document.querySelector('.message').textContent = '🎉 Correct Number!';
+// //changing value of p with class message to 🎉 Correct Number!
+
+// console.log(document.querySelector('.message').textContent);
+
+// //returns ' 🎉 Correct Number! ' new value
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 23;
+
+// console.log(document.querySelector('.guess').value);
+// //returns 23
+
+////////////////////////
+// 1. Event listener
+
